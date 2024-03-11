fbde452e8081fbae9d346416f479e24bf1236b62
Jose Maria Novillo <josenovillo88@gmail.com>
2024-03-11 10:52:54 -0300

    main + sidenav + about us section (responsive design)

---
+++ src/components/Main.jsx
@@ -0,0 +1,28 @@
+const Main = () => {
+    return (
+
+        <>
+        
+        <div  id='main' className="flex items-center justify-center text-white font-bold ">
+            <div className="absolute text-center ">
+                <p className="mt-[100px] text-xl">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum</p>
+                <button className="bg-green-700 hover:bg-green-800 text-white font-bold py-2 px-4 rounded mt-5">Read More</button>
+            </div>
+
+            
+            <img className="h-screen object-cover object-left " src="https://s1.1zoom.me/big0/892/Coffee_Grain_Wood_planks_564164_1280x720.jpg" alt="" />
+            
+            
+            
+        </div>
+
+
+        
+        
+        
+        </>
+        
+    )
+}
+
+export default Main
